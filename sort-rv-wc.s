58a2b981a630bad6d45fff8a95e0cfe859495bf5
Victor Alfaro <142273132+gitonina@users.noreply.github.com>
2023-11-15 13:11:05 -0300

    Add files via upload

---
+++ sort-rv-wc.s
@@ -0,0 +1,142 @@
+#sort: Ordena ascendentemente un arreglo de enteros sin signo usando un
+# algoritmo ridiculamente ineficiente.
+
+# La funcion sort esta programada en assembler RiscV. El codigo equivalente
+# en C esta comentado, mostrando la ubicacion de las variables en los
+# registros.  La funcion recorre el arreglo revisando que los elementos
+# consecutivos esten ordenados.  Si encuentra 2 elementos consecutivos
+# desordenados, los intercambia y reinicia el recorrido del arreglo
+# desde el comienzo.  El arreglo esta ordenado cuando se recorre
+# completamente sin encontrar elementos consecutivos desordenados.
+
+    .file "sort-rv.s"
+    .text
+    .globl sort         # Se necesita para que la etiqueta sea conocida en
+                        # test-sort.c
+    .type sort, @function # typedef unsigned int uint;
+sort:              
+     # void sort(uint nums[], int n) { // registros a0, a1
+    addi    sp,sp,-64   #   // Apila reg
+    sw      ra, 60(sp)  #   // resguarda direccion de retorno
+    sw      a0,56(sp)   #   uint *p= nums; // p esta en sp+56
+    addi    a1,a1,-1    #   uint *ult= &nums[n-1]; // ult esta en sp+52
+    slli    a1,a1,2     #   // tamanno del arreglo
+    add     a1,a0,a1
+    sw      a1,52(sp)
+    sw      a0,48(sp)   #   // nums esta en direccion sp+48
+    mv      t0,a0       #   // p esta en registro t0
+                        #   while (p<ult) {
+    j       .while_cond #     // la condicion del while se evalua al final
+.while_begin:           #     // del ciclo para mayor eficiencia
+
+    sw      t0,56(sp)   # resguardar p en memoria
+
+    # Hasta aca no puede modificar nada
+
+    #################################################
+    ### Comienza el codigo que Ud. debe modificar ###
+    #################################################
+
+   
+     # no puede alterar los registros s0-s11, si lo hace debe resguardarlos
+    # en 0(sp), 4(sp), ... o 44(sp)
+    # El valor de p está temporalmente en el registro t0
+    # No puede hacer más trabajo que la comparación (no puede usar ret)
+
+    lw      a0, 0(t0)    # Carga la dirección de la primera cadena en a0
+    lw      a1, 4(t0)    # Carga la dirección de la segunda cadena en a1
+
+    mv	a5,a0
+	lbu	a4,0(a0)   #cargo el valor del primer caracter de la cadena en a4
+	beq	a4,zero,.L8
+	li	a0,0   #contador de palabras para p[0]
+	li	a6,32 #espacio vacio
+	j	.L7
+.L12:
+	addi	a0,a0,1
+	lbu	a4,0(a5)
+	beq	a4,zero,.L1
+.L4:
+	beq	a4,a6,.L6
+	addi	a5,a5,1
+	lbu	a4,0(a5)
+	bne	a4,zero,.L4
+.L1:
+	j .P1
+.L6:
+	lbu	a4,0(a5)
+	beq	a4,zero,.L11
+.L7:
+	bne	a4,a6,.L12
+	addi	a5,a5,1
+	j	.L6
+.L11:
+	j .P1
+.L8:
+	li	a0,0
+
+
+.P1: 
+    mv	a5,a1
+	lbu	a4,0(a1)
+	beq	a4,zero,.R7
+	li	a1,0  #contador de palabras para p[1]
+	j	.R5
+.R1:
+	addi	a1,a1,1
+	lbu	a4,0(a5)
+	beq	a4,zero,.L1
+.R2:
+	beq	a4,a6,.R4
+	addi	a5,a5,1
+	lbu	a4,0(a5)
+	bne	a4,zero,.R2
+.R3:
+	j .end
+.R4:
+	lbu	a4,0(a5)
+	beq	a4,zero,.R6
+.R5:
+	bne	a4,a6,.R1
+	addi	a5,a5,1
+	j	.R4
+.R6:
+	j .end
+.R7:
+	li	a1,0
+.end:
+
+    # Calcula y almacena la diferencia en t1
+    sub     t1, a0, a1   # Calcula la diferencia entre los contadores de palabras de las dos cadenas
+
+    #################################################
+    ### Fin del codigo que Ud. debe modificar     ###
+    #################################################
+
+    # Desde aca no puede modificar nada
+    # Si t1>0 se intercambian p[0] y p[1] y se asigna p= noms para revisar
+    # nuevamente que los elementos esten ordenados desde el comienzo del arreglo
+
+
+
+.decision:              #     if (0>=rc) {
+    lw      t0,56(sp)   #       // p esta en registro t0
+    blt     zero,t1,.else
+    addi    t0,t0,4     #       p++; // avanzar en arreglo de enteros
+    j       .while_cond #     }
+
+.else:                  #     else { // intercambar p[0] y p[1], y reiniciar
+    lw      a0,0(t0)    #       int aux= p[0]; // a0
+    lw      a1,4(t0)    #       int aux2= p[1];
+    sw      a0,4(t0)    #       p[0]= aux2;
+    sw      a1,0(t0)    #       p[1]= aux;
+    lw      t0,48(sp)   #       p= noms;
+                        #     }
+
+.while_cond:            #     // se evalua la condicion del while
+    lw      t1,52(sp)   #     // ult esta en t1
+    bltu    t0,t1,.while_begin #  // Condicion del while es p<ult
+			#   }
+    lw      ra,60(sp)   #   // Se restaura direccion de retorno
+    addi    sp,sp,64    #   // Desapila registro de activacion
+    ret			# }
